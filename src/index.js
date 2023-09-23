11b4493e61ce6e9a5c168f190ced4ee721aa3e44
Kymani Stephens <kymanist00@gmail.com>
2023-09-23 14:59:45 -0700

    add body event listener for testing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-import { openProjectModal, closeProjectModal } from "./DOM";
+import { openProjectModal, closeProjectModal, submitProjectModal } from "./DOM";
+import { getAllProjectsData } from "./modules/projectManager";
 
 const addProjectBtn = document.querySelector('#add-project-btn')
 const closeProjectBtn = document.querySelector('#close-project-btn')
+const submitProjectBtn = document.querySelector('#submit-project-btn')
 
 addProjectBtn.addEventListener('click', openProjectModal)
 closeProjectBtn.addEventListener('click', closeProjectModal)
+submitProjectBtn.addEventListener('click', submitProjectModal)
+
+
+document.body.addEventListener('click', () => {console.log(getAllProjectsData())})
