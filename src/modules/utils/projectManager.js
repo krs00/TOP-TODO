75b71201c35ac2addff37417da9c2a575be3442e
Kymani Stephens <kymanist00@gmail.com>
2023-09-26 18:47:19 -0700

    add project manager obj

--- src/modules/utils/projectManager.js
+++ src/modules/utils/projectManager.js
@@ -1,6 +1,66 @@
 const projectManager = {
-  projectCounter: 0,
+  projectIdCounter: 0,
   projectArray: [],
   currentProject: null,
+};
 
+export const getProjectArray = () => {
+  projectManager.projectArray;
+  return projectManager.projectArray; 
+};
+
+export const addNewProject = (obj) => {
+  projectManager.projectArray.push(obj);
+  incrementProjectIdCounter()  
+};
+
+export const removeProject = (id) => {
+  const array = projectManager.projectArray
+
+  const newArray = array.filter(item => item.id !== id);
+
+  projectManager.projectArray = newArray
+
+};
+
+export const getNewProjectId = () => {
+  return projectManager.projectIdCounter
+};
+
+const incrementProjectIdCounter = () => {
+  projectManager.projectIdCounter ++;
+};
+
+export const updateProjectInfo = (id, title, desc) => {
+
+  const array = projectManager.projectArray
+
+  for (let i = 0; i < array.length; i++) {
+
+    const project = array[i]
+
+    if (project.id === id) {
+
+      project.title = title
+      project.description = desc
+    }
+  }
+}
+
+export const updateCurrentProject = (id) => {
+  const array = projectManager.projectArray
+
+  for (let i = 0; i < array.length; i++) {
+
+    const project = array[i]
+
+    if (project.id === id) {
+
+      projectManager.currentProject = project
+    }
+  }
+}
+
+export const getCurrentProject = () => {
+  return projectManager.currentProject
 }
