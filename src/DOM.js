2de1d6dc0540b09cefbabd05edf7cac392d294a9
Kymani Stephens <kymanist00@gmail.com>
2023-09-27 17:26:01 -0700

    create new DOM element for project on modal submit

--- src/DOM.js
+++ src/DOM.js
@@ -1,3 +1,7 @@
+import { projectFactory } from "./modules/utils/projectFactory"
+import { addNewProject, getNewProjectId, getProjectArray } from "./modules/utils/projectManager"
+import { projectBox } from "./modules/components/projectBox"
+
 function openOverlay() {
   const overlay = document.querySelector('#overlay')
   overlay.style.display = 'block'
@@ -19,6 +23,36 @@ export function closeProjectModal() {
   closeOverlay()
   const modal = document.querySelector('#add-project-modal')
   modal.style.display = 'none'
+}
+
+function clearProjectModalInputs() {
+  const projectName = document.querySelector('#project-name-input')
+  const projectDescription = document.querySelector('#project-description-input')
+
+  projectName.value = ''
+  projectDescription.value = ''
+
+}
+
+export function submitProjectModal() {
+  const projectName = document.querySelector('#project-name-input').value
+  const projectDescription = document.querySelector('#project-description-input').value
+
+  console.log(projectName)
+  console.log(projectDescription)
+
+  const newId = getNewProjectId()
+
+  const newProject = projectFactory(newId, projectName, projectDescription)
+  projectBox(newId, projectName)
+  addNewProject(newProject) 
+
+
+  console.log(getProjectArray())
+
+  clearProjectModalInputs()
+  closeProjectModal() 
+
 }
 
 
