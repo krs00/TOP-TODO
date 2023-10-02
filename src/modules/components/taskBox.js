f854ae7bfafe21e5f8230664e97549f0207e871d
Kymani Stephens <kymanist00@gmail.com>
2023-10-02 15:25:37 -0700

    finish dynamic task box component

--- src/modules/components/taskBox.js
+++ src/modules/components/taskBox.js
@@ -1,4 +1,4 @@
-export function taskBox(id, projectId, title, description, dueDate) {
+export function taskBox(title, description, dueDate) {
     const taskBody = document.createElement('div')
 
     // creates body
@@ -6,39 +6,41 @@ export function taskBox(id, projectId, title, description, dueDate) {
 
     //create wrapper for body btns
     const btnWrapper = document.createElement('div')
-    btnWrapper.classList.add('main-task-line-wrapper') 
+    btnWrapper.classList.add('main-task-line-wrapper')
+    taskBody.appendChild(btnWrapper) 
 
     // create edit btn
     const editBtn = document.createElement('div')
+    editBtn.innerText = 'edit'
     editBtn.classList.add('main-edit-btn')
     
      // create del btn
      const delBtn = document.createElement('div')
+     delBtn.innerText = 'del'
      delBtn.classList.add('main-del-btn')  
 
      btnWrapper.appendChild(editBtn)
      btnWrapper.appendChild(delBtn)
 
-     taskBody.appendChild(btnWrapper)
-
      // main title
-     mainTitle = document.createElement('p')
+     const mainTitle = document.createElement('p')
      mainTitle.classList.add('main-task-title')
      mainTitle.innerText = title
      taskBody.appendChild(mainTitle)
 
     // duetext
-    duetext = document.createElement('p')
+    const duetext = document.createElement('p')
     duetext.innerText = `due date: ${dueDate}`
     taskBody.appendChild(duetext)
 
     // main title
-    descText = document.createElement('p')
+    const descText = document.createElement('p')
     descText.innerText = description
     taskBody.appendChild(descText)
 
     const container = document.querySelector('#task-list')
 
     container.appendChild(taskBody)
+    console.log('test taskbox')
   }
 
